3df9d03fa4c735ff95e9b1bde984feb455a9891e
handomitoty26 <cola843@gmail.com>
2026-06-22 15:55:28 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - 태하관퇴실체크아웃v1 Service Worker
-const CACHE_NAME = 'taehagwan-checkout-v3';
+const CACHE_NAME = 'taehagwan-checkout-v4';
 const ASSETS = [
   './index.html',
   './manifest.json',
